1be6a2f42a701eef80bc190321cd116fdbb83606
Nathaniela Aptanta Parama <terubozu119@gmail.com>
2025-12-07 17:29:45 +0700

    ✏️ change name class

--- src/components/main/Footers.tsx
+++ src/components/main/Footers.tsx
@@ -2,13 +2,13 @@ interface Props {
     darkMode: boolean;
 }
 
-export default function Footer({darkMode}: Props) {
+export default function Footers({darkMode}: Props) {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={`mt-2 ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-300'}`}>
       <div className="text-center text-sm text-gray-500">
-        &copy; {currentYear} <a href="https://github.com/donaptello">Nathan Parama</a>. All rights reserved.
+        &copy; {currentYear} <a href="https://github.com/donaptello" target="_blank" className="font-semibold underline text-gray-900">Nathan Parama</a>. All rights reserved.
       </div>
     </footer>
   );
