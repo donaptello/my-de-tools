28a00eb8b48988700ccd6f6795f5dc1fc83a9b2e
Nathaniela Aptanta Parama <terubozu119@gmail.com>
2025-12-13 11:26:51 +0700

    ✅ update main route

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ToolsTables from './pages/tools-tables/ToolsTables.tsx'
+import Monitoring from './pages/monitoring/Monitoring.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/tools" element={<ToolsTables />} />
+          <Route path="/monitoring" element={<Monitoring />} />
+        </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
