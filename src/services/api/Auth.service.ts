bc02abdb95f5dd3cd01cfe5e4b53958edeff6f4c
Nathaniela Aptanta Parama <terubozu119@gmail.com>
2026-02-16 19:09:23 +0700

    ✅ mapping login response from backend

--- src/services/api/Auth.service.ts
+++ src/services/api/Auth.service.ts
@@ -3,8 +3,11 @@ import { apiDeTools } from "./Http";
 
 export const authService = {
   async login(payload: LoginAuthPayload): Promise<AuthTokenRes> {
-    return apiDeTools
-      .post("/v1/auth/login", payload)
-      .then((res) => res.data);
+    return apiDeTools.post("/v1/auth/login", payload).then((res) => {
+      const tokenAuth: AuthTokenRes = {
+        accessToken: res.data.access_token,
+      };
+      return tokenAuth;
+    });
   },
 };
