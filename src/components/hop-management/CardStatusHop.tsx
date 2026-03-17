7fd51d88d060b9e308ffcf284ec94cd19e5f4da8
Nathaniela Aptanta Parama <terubozu119@gmail.com>
2026-03-17 10:04:45 +0700

    ✨ add card status hop server

---
+++ src/components/hop-management/CardStatusHop.tsx
@@ -0,0 +1,146 @@
+import { Cpu, HardDrive, Server } from "lucide-react";
+import { HopStatus } from "../../services/types/HopManagement.types";
+
+type CardStatusHopProps = {
+  darkMode: boolean;
+  hopStatus?: HopStatus | undefined;
+};
+
+export default function CardStatusHop({
+  darkMode,
+  hopStatus,
+}: CardStatusHopProps) {
+  const memoryPercent =
+    ((hopStatus?.memoryUsed ?? 0) / (hopStatus?.memoryTotal ?? 1)) * 100;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-3 items-stretch">
+      {/* <div
+        className={`rounded-xl border  p-4 shadow-sm transition hover:shadow-2xl hover:-translate-y-1 ${
+          darkMode ? "bg-gray-800" : "border-gray-200 bg-white"
+        }`}
+      > */}
+      {/* Hop Server */}
+      <div
+        className={`rounded-xl border p-5 shadow-sm ${
+          darkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"
+        }`}
+      >
+        <div className="flex items-center gap-2 mb-6">
+          <Server size={18} className={`text-muted-foreground ${darkMode ? "text-gray-400": ""}`} />
+          <h3 className={`tracking-tight text-sm font-medium flex items-center gap-2 ${darkMode ? "text-gray-200" : ""}`}>
+            Hop Server
+          </h3>
+        </div>
+
+        <div className="space-y-4 text-sm">
+          <div className="flex justify-between">
+            <span className="text-gray-500 text-xs">Status</span>
+            <span className="px-3 py-0.5 text-xs rounded-full bg-green-100 text-green-600">
+              {hopStatus?.statusHop}
+            </span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className="text-gray-500 text-xs">Uptime</span>
+            <span>{hopStatus?.uptime}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className="text-gray-500 text-xs">Threads</span>
+            <span>{hopStatus?.cpuCores}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className="text-gray-500 text-xs">Load Avg</span>
+            <span>{hopStatus?.loadAvg}</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Memory */}
+      <div
+        className={`rounded-xl border p-6 shadow-sm ${
+          darkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"
+        }`}
+      >
+        <div className="flex items-center gap-2 mb-4">
+          <HardDrive size={18} className={`text-muted-foreground ${darkMode ? "text-gray-400": ""}`} />
+          <h3 className={`tracking-tight text-sm font-medium flex items-center gap-2 ${darkMode ? "text-gray-200" : ""}`}>
+            Memory Usage
+          </h3>
+        </div>
+
+        <div className="flex justify-between text-sm mb-2">
+          <span className="text-gray-500 text-xs">Used</span>
+          <span className="text-xs">
+            {hopStatus?.memoryUsed.toFixed(2)} /{" "}
+            {hopStatus?.memoryTotal.toFixed(2)} GB
+          </span>
+        </div>
+
+        <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden mb-4">
+          <div
+            className="h-full bg-blue-500"
+            style={{ width: `${memoryPercent}%` }}
+          />
+        </div>
+
+        <div className="grid grid-cols-2 gap-2">
+          <div className="rounded-lg bg-gray-100 p-4 text-center">
+            <p className="text-xs text-gray-500">Free</p>
+            <p className="text-green-500 font-semibold">
+              {hopStatus?.memoryFree.toFixed(2)} GB
+            </p>
+          </div>
+
+          <div className="rounded-lg bg-gray-100 p-4 text-center">
+            <p className="text-xs text-gray-500">Used</p>
+            <p className="text-red-500 font-semibold">
+              {hopStatus?.memoryUsed.toFixed(2)} GB
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* CPU */}
+      <div
+        className={`rounded-xl border p-6 shadow-sm ${
+          darkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"
+        }`}
+      >
+        <div className="flex items-center gap-2 mb-6">
+          <Cpu size={18} className={`text-muted-foreground ${darkMode ? "text-gray-400": ""}`} />
+          <h3 className={`tracking-tight text-sm font-medium flex items-center gap-2 ${darkMode ? "text-gray-200" : ""}`}>
+            CPU Info
+          </h3>
+        </div>
+
+        <div className="space-y-4 text-sm">
+          <div className="flex justify-between items-center">
+            <span className="text-gray-500 text-xs">Cores</span>
+            <span className="text-3xl font-bold">{hopStatus?.cpuCores}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className="text-gray-500 text-xs">Load Average</span>
+            <span>{hopStatus?.loadAvg}</span>
+          </div>
+
+          <div>
+            <div className="flex justify-between text-sm mb-1">
+              <span className="text-gray-500 text-xs">CPU Load</span>
+              <span>{hopStatus?.cpuCores}%</span>
+            </div>
+
+            <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+              <div
+                className="h-full bg-blue-500"
+                style={{ width: `${hopStatus?.loadAvg}%` }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/hop-management/HopManagement.tsx
+++ src/pages/hop-management/HopManagement.tsx
@@ -1,14 +1,20 @@
 import { useOutletContext } from "react-router-dom";
 import { LayoutContextType } from "../../components/main/Layout";
 import { useEffect } from "react";
+import CardStatusHop from "../../components/hop-management/CardStatusHop";
+import { useHopManagementStatus } from "../../services/hooks/useHopManagement";
 
 export default function HopManagement() {
-  const { darkMode, setTitle } = useOutletContext<LayoutContextType>();
+  const { darkMode, setTitle, setDesc } = useOutletContext<LayoutContextType>();
+  const { data: status } = useHopManagementStatus();
 
   useEffect(() => {
     setTitle("Hop Management");
+    setDesc("Apache Hop monitoring overview");
   });
   return (
-    <div className="grid grid-cols-1 px-10 md:px-40 flex-1 items-stretch"></div>
+    <div className="grid grid-cols-1 px-10 md:px-40 flex-1 items-stretch">
+      <CardStatusHop darkMode={darkMode} hopStatus={status?.data} />
+    </div>
   );
 }
