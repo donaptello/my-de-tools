55066fea861eba3a9566d2cef34d62e245327a2b
Nathaniela Aptanta Parama <terubozu119@gmail.com>
2026-03-27 00:58:53 +0700

    ✅ convert format date

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -8,3 +8,23 @@ export function timeCheckerExpAuth(exp: number): boolean {
   }
   return false;
 }
+
+export function formatDate(
+  dateString: string,
+  type: string | undefined = undefined,
+): string {
+  const date = new Date(dateString);
+
+  const yyyy = date.getFullYear();
+  const MM = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDay()).padStart(2, "0");
+
+  const HH = String(date.getHours()).padStart(2, "0");
+  const mm = String(date.getMinutes()).padStart(2, "0");
+  const ss = String(date.getSeconds()).padStart(2, "0");
+
+  if (type !== undefined && type === "yy/mm/dd") {
+    return `${dd}/${MM}/${yyyy}`;
+  }
+  return `${yyyy}-${MM}-${dd} ${HH}:${mm}:${ss}`;
+}
